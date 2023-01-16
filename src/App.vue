28c1e10d987a6cc0ec01d7e447af6b64a63e27ea
kou256 <25428510+kou256@users.noreply.github.com>
2023-01-16 14:21:18 +0900

    ✨ アプリのタイトルを表示するためのheaderを実装
    header用のコンポーネントを実装しタイトルを表示する。

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <TheHeader title="YumemiApp" />
     <PrefecturesList />
   </div>
 </template>
 <script lang="js">
+import TheHeader from './components/modules/TheHeader.vue';
 import PrefecturesList from './components/PrefecturesList.vue';
 export default {
   name: 'App',
   components: {
+    TheHeader,
     PrefecturesList
   },
 }
